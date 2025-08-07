85cee1e62fa878d8a114733390500be4eebeb713
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-08-07 17:59:00 +0530

    cinditions in useState

--- vite/vite-project-1/src/App.jsx
+++ vite/vite-project-1/src/App.jsx
@@ -4,7 +4,7 @@ import Product from "./Product";
 export default function App() {
   var [a,b] = useState(10)
   return (
-    <div className="h-screen bg-green-500 text-white flex flex-col justify-center items-center gap-4">
+    <div className="h-screen bg-gradient-to-br from-[#F8F4F0] via-[#DDD0C8] to-[#B0A89F] text-white flex flex-col justify-center items-center gap-4">
       <h1 className="text-4xl font-bold">Welcome to React 🎉</h1>
       <button onClick={()=>{b(a+10)}} className="px-6 py-2 bg-blue-600 hover:bg-blue-800 rounded-md">
         Click Me
--- vite/vite-project-1/src/Product.jsx
+++ vite/vite-project-1/src/Product.jsx
@@ -2,8 +2,11 @@ import React, { useState } from 'react'
 
 function Product({productValue}) {
 
+  const [a,b] = useState(false);
   return (
-    <div className='text-gray-950 font-bold'>Number {productValue}</div>
+    <div className='text-gray-950 font-bold'>
+      <button onClick={()=>{b(!a)}} className={`${a=== false ? "text-red-600" : "text-green-600"}`}>{a === false ? "Flase" : "True"}</button>
+    </div>
   )
 }
 
