503f70876864320f41bcfb86e39f898d98b62a12
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-08-07 10:54:02 +0530

    created and added new component "Product"

--- vite/vite-project-1/src/App.jsx
+++ vite/vite-project-1/src/App.jsx
@@ -1,3 +1,5 @@
+import Product from "./Product";
+
 export default function App() {
   return (
     <div className="h-screen bg-green-500 text-white flex flex-col justify-center items-center gap-4">
@@ -5,6 +7,7 @@ export default function App() {
       <button className="px-6 py-2 bg-blue-600 hover:bg-blue-800 rounded-md">
         Click Me
       </button>
+      <Product/>
     </div>
   );
 }
---
+++ vite/vite-project-1/src/Product.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Product() {
+  return (
+    <div className='text-gray-950 font-bold'>New Product</div>
+  )
+}
+
+export default Product
