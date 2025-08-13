50a7d42df02f155103bda116a0eb46db5c6d39b9
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-08-13 18:31:31 +0530

    Username Generator

---
+++ vite/vite-2/react_basic/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+function App(){
+  var [a, setA] = useState("abhishek01")
+  const age = 23;
+  const user = "Abhishek"
+  // var userName = "abhishek01"
+  const changeUsername = ()=>{
+      setA(`abhishek${Math.floor(Math.random()*1000)}`);
+  }
+  return (
+  <>
+  <h1>Hello {user}</h1>
+  <h2> Your username is- {a}</h2>
+  <button onClick={changeUsername}>Change username</button>
+  </>)
+}
+
+export default App;
