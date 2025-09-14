80a53370a5c816d85f4ab2062ab22d896394db04
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-09-14 19:34:42 +0530

    UseState concept

--- Basic/test/concept-testing/src/App.jsx
+++ Basic/test/concept-testing/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import UserData from './concepts/props/UserData'
+import UserData from './concepts/props/UserData';
+import UseStateExample from './concepts/usestate/UseStateExample';
 
 const App = () => {
   return (
     <div>
-      <UserData/>
+      {/* <UserData/> */}
+      <UseStateExample/>
     </div>
   )
 }
