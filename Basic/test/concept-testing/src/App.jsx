fd4bd85d802c98d63d3544c12b86fa62332a7114
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-09-14 21:10:16 +0530

    Event Handling

--- Basic/test/concept-testing/src/App.jsx
+++ Basic/test/concept-testing/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import UserData from './concepts/props/UserData';
 import UseStateExample from './concepts/usestate/UseStateExample';
+import ButtonClick from './concepts/event-handling/ButtonClick';
 
 const App = () => {
   return (
     <div>
       {/* <UserData/> */}
-      <UseStateExample/>
+      {/* <UseStateExample/> */}
+      <ButtonClick/>
     </div>
   )
 }
