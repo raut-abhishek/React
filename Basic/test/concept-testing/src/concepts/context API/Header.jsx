dddb0ea0fd03b3b1754bb7a488956c4d27df5f22
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-09-23 18:27:03 +0530

    toggle theme button added

--- Basic/test/concept-testing/src/concepts/context API/Header.jsx
+++ Basic/test/concept-testing/src/concepts/context API/Header.jsx
@@ -4,7 +4,13 @@ import ThemeContext from "./ThemeContext";
 
 const Header = () => {
   const { theme } = useContext(ThemeContext);
-  return <h1>Header - Current Theme: {theme}</h1>;
+  const { toggleTheme } = useContext(ThemeContext);
+  return (
+  <>
+  <h1>Header - Current Theme: {theme}</h1>
+  <button onClick={toggleTheme}>Toggle Theme</button>
+  </>
+  );
 };
 
 export default Header;
